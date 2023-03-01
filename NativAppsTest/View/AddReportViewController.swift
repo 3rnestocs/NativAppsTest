ea8271efe3f9b5c287bcf9f62c529e9caf3a06f5
3rnestocs <ernesto.contreras@drapshop.com>
2023-03-01 13:52:14 -0400

    Logic to take picture from camera added. Validations for report info added.

--- NativAppsTest/View/AddReportViewController.swift
+++ NativAppsTest/View/AddReportViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class AddReportViewController: UIViewController {
 
-    // MARK: - Constants
-    
+    // MARK: - Outlet
     @IBOutlet private(set) weak var descriptionTextField: TextField!
     @IBOutlet private(set) weak var descriptionLabel: UILabel!
     @IBOutlet private(set) weak var titleLabel: UILabel!
     @IBOutlet private(set) weak var imageButton: UIButton!
     @IBOutlet private(set) weak var imageView: UIImageView!
-    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet private(set) weak var saveButton: UIButton!
     @IBOutlet private(set) weak var imageContainerView: UIView!
-    // MARK: - Properties
+    @IBOutlet weak var descriptionAlertLabel: UILabel!
+    @IBOutlet weak var imageAlertLabel: UILabel!
+    
+    private var currentImage: UIImage? {
+        didSet {
+            if let image = currentImage {
+                self.imageView.image = image
+            }
+        }
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -32,33 +40,99 @@ class AddReportViewController: UIViewController {
     }
     
     private func setupUI() {
+        view.layer.cornerRadius = 40
         setupTextField()
         setupButtons()
+        setupGesture()
+        setupImage()
+    }
+    
+    private func setupGesture() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        self.view.addGestureRecognizer(gesture)
     }
     
     private func setupTextField() {
+        self.descriptionTextField.delegate = self
         self.descriptionTextField.attributedPlaceholder = NSAttributedString(string: "Descripción", attributes: [.foregroundColor: UIColor.lightGray])
-        self.descriptionTextField.layer.cornerRadius = 24
+        self.descriptionTextField.layer.cornerRadius = 20
         self.descriptionTextField.layer.borderColor = UIColor.black.cgColor
         self.descriptionTextField.layer.borderWidth = 1
     }
     
     private func setupButtons() {
-        self.imageButton.layer.cornerRadius = 24
+        self.imageButton.layer.cornerRadius = 20
         self.imageButton.layer.borderColor = UIColor.black.cgColor
         self.imageButton.layer.borderWidth = 1
         
-        self.saveButton.layer.cornerRadius = 24
+        self.saveButton.layer.cornerRadius = 20
         self.saveButton.layer.borderColor = UIColor.black.cgColor
         self.saveButton.layer.borderWidth = 1
+    }
+    
+    private func setupImage() {
+        self.imageView.layer.cornerRadius = 60
     }
     
     // MARK: - Helpers
     
     // MARK: - Actions
-    @IBAction func imageButtonTapped(_ sender: UIButton) {
+    @objc private func viewTapped() {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction private func imageButtonTapped(_ sender: UIButton) {
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
     }
 
-    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    @IBAction private func saveButtonTapped(_ sender: UIButton) {
+        var canSave: Bool = false
+        if let textField = descriptionTextField.text, !textField.isEmpty {
+            descriptionAlertLabel.isHidden = true
+            canSave = true
+        } else {
+            descriptionAlertLabel.isHidden = false
+            canSave = false
+        }
+        
+        if currentImage != nil {
+            imageAlertLabel.isHidden = true
+            canSave = true
+        } else {
+            imageAlertLabel.isHidden = false
+            canSave = false
+        }
+        
+        if canSave {
+            guard let text = descriptionTextField.text else { return }
+            let report = Report(image: nil, description: text)
+            print("T3ST save report", report)
+        }
+    }
+}
+
+extension AddReportViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        descriptionAlertLabel.isHidden = true
+        return true
+    }
+}
+
+extension AddReportViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            fatalError("There was no image found.")
+        }
+        self.currentImage = image
     }
 }
