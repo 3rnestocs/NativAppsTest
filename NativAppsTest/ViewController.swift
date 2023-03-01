b9a833f9b3c0ea38a8d51b013ad872f43ce41316
3rnestocs <ernesto.contreras@drapshop.com>
2023-03-01 11:41:47 -0400

    Initial setup.

---
+++ NativAppsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NativAppsTest
+//
+//  Created by Ernesto Jose Contreras Lopez on 1/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
